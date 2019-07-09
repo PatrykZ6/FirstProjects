9cc483fbdc8e6de9f4e2580148bdc450250e8c60
PatrykZ6 <patrykzdunek6@gmail.com>
2019-07-09 19:12:48 +0200

    nowe zmiany + dodatkowy plik

--- Python/witaj.py
+++ Python/witaj.py
@@ -3,4 +3,5 @@ waga = 63
 wiek = 25
 S = -161
 PPM = 10 * waga * 6.25 * wzrost + 5 * wiek + S
-Print("Dzienne zapotrzebowanie kaloryczne wynosi:", PPM * 1.6, "kcal")
+PPMReal = PPM * 1.6
+print("Dzienne zapotrzebowanie kaloryczne wynosi: {:.2f} kcal".format(PPMReal))
