ff83d3fc13e395ff07bb11b283fd70c316dbeb02
Patryk <52633247+PatrykZ6@users.noreply.github.com>
2019-07-12 15:49:34 +0200

    Create Metody_zwracajace_nowy_napis_(string).py

---
+++ Python/Metody_zwracajace_nowy_napis_(string).py
@@ -0,0 +1,14 @@
+text = "lubie Jesc KaBanosy"
+
+print ("Oryginalny tekst: ",text,"\n")
+
+print ("{:20s}".format("text.lower():"),text.lower())
+print ("{:20s}".format("text.upper():"),text.upper())
+print ("{:20s}".format("text.capitalize():"),text.capitalize())
+print ("{:20s}".format("text.title():"),text.title())
+print ("{:20s}".format("' '.join(text):")," ".join(text))
+print ("lub:")
+print ("{:20s}".format("'*'.join(text):"),"*".join(text))
+#print ("{:20s}".format("text.: "),text.)
+print ("{:20s}".format("text.lstrip(): "),text.lstrip())
+print ("{:20s}".format(": "),)
