040f29f2c858687c9564982d01ae01841f657581
Patryk <52633247+PatrykZ6@users.noreply.github.com>
2019-09-19 15:19:00 +0200

    Pętla for, która dla 10 kolejnych liczb naturalnych wyświetli ich wartość do sześcianu
    Napisz program, który dla 10 kolejnych liczb naturalnych wyświetli ich wartość do potęgi trzeciej.

---
+++ Python/Zadania/FlyNerd - Python #7, Zadanie 2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf8 -*-
+
+"""
+Napisz program, który dla 10 kolejnych liczb naturalnych wyświetli ich ich wartość do sześcianu
+
+"""
+
+for i in range(11000000):
+  x = i**3 
+  print (i,'^3 =',x)
