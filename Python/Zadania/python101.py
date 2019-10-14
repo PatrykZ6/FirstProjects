c31c31907f8894e60f8a5ee3f76a6f161ac9b43a
Patryk <52633247+PatrykZ6@users.noreply.github.com>
2019-10-14 16:03:00 +0200

    Create python101.py

---
+++ Python/Zadania/python101.py
@@ -0,0 +1,19 @@
+# -*- coding: utf8 -*-
+
+import random
+
+ileliczb = int(input("Podaj ilość typowanych liczb: "))
+maksliczba = int(input("Podaj maksymalną losowaną liczbę: "))
+print ("Wytypuj {} z {} liczb".format(ileliczb,maksliczba))
+
+liczby = []
+i=0
+while i < ileliczb:
+  los = random.randint(1, maksliczba)
+  print (los)
+  print (liczby.count(los) == 0)
+  if liczby.count(los) == 0:
+    liczby.append(los)
+    i+=1
+    
+print (liczby)
