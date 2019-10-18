b8339ea384d0f6fab9f0479d9bd346501df044c5
Patryk <52633247+PatrykZ6@users.noreply.github.com>
2019-10-18 15:12:13 +0200

    Duży lotek

--- Python/Zadania/python101.py
+++ Python/Zadania/python101.py
@@ -1,33 +1,23 @@
 # -*- coding: utf8 -*-
 
-
 import random
-"""
-liczby = []
-i=0
-while i < ileliczb:
-  los = random.randint(1, maksliczba)
-  if ileliczb>maksliczba:
-    print ("Nie możesz podać maksymalnej liczby mniejszej niż ilość typowanych liczb.")
-    break
-  elif liczby.count(los) == 0:
-    liczby.append(los)
-    i+=1
-if liczby != []:
-  print (liczby)  
-"""
-##################################################
-ileliczb = 0
-while ileliczb<=0:
-  ileliczb = int(input("Podaj ilość typowanych liczb: "))
-  if ileliczb<=0:
-    print ("Ilość typowanych liczb nie może być równa zeru ani nie może być liczbą ujemną")
 
-maksliczba = 0
-while ileliczb>maksliczba:
-  maksliczba = int(input("Podaj maksymalną losowaną liczbę: "))
-  if ileliczb>maksliczba:
-    print ("Nie możesz podać maksymalnej liczby mniejszej niż ilość typowanych liczb.")
+try:
+  ileliczb = 0
+  while ileliczb<=0:
+    ileliczb = int(input("Podaj ilość typowanych liczb: "))
+    if ileliczb<=0:
+     print ("Ilość typowanych liczb nie może być równa zeru ani nie może być liczbą ujemną")
+
+  maksliczba = 0
+  while ileliczb>maksliczba:
+    maksliczba = int(input("Podaj maksymalną losowaną liczbę: "))
+    if ileliczb>maksliczba:
+     print ("Nie możesz podać maksymalnej liczby mniejszej niż ilość typowanych liczb.")
+except ValueError:
+  print("Błędne dane!")
+  exit()
+
 
 liczby = set()
 i=0
@@ -56,7 +46,7 @@ for z in range(3):
   #print(trafione)
   print ("Trafione liczby: {}. Te liczby to: {}".format(len(trafione),(", ".join(str(x) for x in trafione))))
   if (set(strzaly) != set(trafione) and z != 2):
-    print("\n####################################################\n\nSpróbuj jeszcze raz!")
+    print("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\nSpróbuj jeszcze raz!")
   elif set(strzaly) == set(trafione):
     print("Brawo! Zgadłeś wszystkie liczby :)")
     break
