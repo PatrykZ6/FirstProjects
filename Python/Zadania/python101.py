48822475f4e8c40633d64e4e5546258a4f2e1daf
Patryk <52633247+PatrykZ6@users.noreply.github.com>
2019-10-18 14:49:50 +0200

    Duży lotek

--- Python/Zadania/python101.py
+++ Python/Zadania/python101.py
@@ -2,11 +2,6 @@
 
 
 import random
-
-ileliczb = int(input("Podaj ilość typowanych liczb: "))
-maksliczba = int(input("Podaj maksymalną losowaną liczbę: "))
-print ("Wytypuj {} z {} liczb".format(ileliczb,maksliczba))
-
 """
 liczby = []
 i=0
@@ -18,38 +13,52 @@ while i < ileliczb:
   elif liczby.count(los) == 0:
     liczby.append(los)
     i+=1
-
 if liczby != []:
   print (liczby)  
 """
 ##################################################
+ileliczb = 0
+while ileliczb<=0:
+  ileliczb = int(input("Podaj ilość typowanych liczb: "))
+  if ileliczb<=0:
+    print ("Ilość typowanych liczb nie może być równa zeru ani nie może być liczbą ujemną")
+
+maksliczba = 0
+while ileliczb>maksliczba:
+  maksliczba = int(input("Podaj maksymalną losowaną liczbę: "))
+  if ileliczb>maksliczba:
+    print ("Nie możesz podać maksymalnej liczby mniejszej niż ilość typowanych liczb.")
 
 liczby = set()
 i=0
 while i < ileliczb:
-  if ileliczb>maksliczba:
-    print ("Nie możesz podać maksymalnej liczby mniejszej niż ilość typowanych liczb.")
-    break  
   los = random.randint(1, maksliczba)
   liczby.add(los)
   i=len(liczby)
 
-if liczby != set():
-  print (liczby)
+print ("Wytypuj {} z {} liczb".format(ileliczb,maksliczba))
+#print (liczby)
 
-strzaly = []
-x=0
-while x < ileliczb:
-  strzal = int(input("Podaj {} liczbę: ".format(x+1)))
-  if strzal < 0 or strzal > maksliczba:
-    print ("Podałeś złą wartość, minimalna wartość to 1 a maksymalna to {}.".format(maksliczba))
-  elif strzaly.count(strzal) != 0:
-    print ("Podałeś jeszcze raz tą samą cyfrę.")
+for z in range(3):
+  strzaly = []
+  x=0
+  while x < ileliczb:
+    strzal = int(input("Podaj {} liczbę: ".format(x+1)))
+    if strzal < 0 or strzal > maksliczba:
+      print ("Podałeś złą wartość, minimalna wartość to 1 a maksymalna to {}.".format(maksliczba))
+    elif strzaly.count(strzal) != 0:
+      print ("Podałeś jeszcze raz tą samą liczbę.")
+    else:
+      strzaly.append(strzal)
+      x+=1
+  trafione = liczby & set(strzaly)
+  #print(strzaly)
+  #print(trafione)
+  print ("Trafione liczby: {}. Te liczby to: {}".format(len(trafione),(", ".join(str(x) for x in trafione))))
+  if (set(strzaly) != set(trafione) and z != 2):
+    print("\n####################################################\n\nSpróbuj jeszcze raz!")
+  elif set(strzaly) == set(trafione):
+    print("Brawo! Zgadłeś wszystkie liczby :)")
+    break
   else:
-    strzaly.append(strzal)
-    x+=1
-
-trafione = liczby & set(strzaly)
-print(strzaly)
-print(trafione)
-print ("Trafiłemś cyfr: {}. Te cyfry to: {}".format(len(trafione),trafione))
+    break
